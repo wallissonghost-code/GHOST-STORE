0e42301ebaefe5743d4f93cf17345a052cef549b
ghostzinho <wallissonghost@gmail.com>
2026-08-27 12:36:47 -0300

    Connect Ghost Store to Supabase

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.GHOST_STORE_CONFIG = {
-  supabaseUrl: '',
-  supabasePublishableKey: '',
+  supabaseUrl: 'https://cylljglldybbgttbokao.supabase.co',
+  supabasePublishableKey: 'sb_publishable_U0O9e8-ZnH8X3AIKGHi-MA_OOKtZ-Ho',
   supabaseAnonKey: '',
   productsTable: 'products',
   adminsTable: 'store_admins',
