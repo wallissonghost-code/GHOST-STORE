1581e257bd01c82688a44e98597ac38bdd93fe01
ghostzinho <wallissonghost@gmail.com>
2026-08-27 12:26:01 -0300

    Keep storefront compatibility with Supabase publishable key

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 window.GHOST_STORE_CONFIG = {
   supabaseUrl: '',
   supabasePublishableKey: '',
+  supabaseAnonKey: '',
   productsTable: 'products',
   adminsTable: 'store_admins',
   storageBucket: 'product-files'
